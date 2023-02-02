5b72393c773ec09be5efd3ce325ed8184e7df57c
gheo <gyucheon.heo@ascension.org>
2023-02-02 10:54:22 -0600

    Add PropertyReader

---
+++ src/main/java/PropertyReader.java
@@ -0,0 +1,28 @@
+import models.Result;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    private final Result<Properties> properties;
+
+    public PropertyReader(String configFileName) {
+        this.properties = readProperties(configFileName);
+    }
+
+    private Result<Properties> readProperties(String configFileName) {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(configFileName)) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            return Result.success(properties);
+        } catch (Exception e) {
+            return Result.failure(e);
+        }
+    }
+
+    public Result<String> getProperty(String name) {
+        return properties.map(props -> props.getProperty(name));
+    }
+
+}
